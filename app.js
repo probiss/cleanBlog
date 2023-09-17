c414c92d6c644bd8a37c7a82d5e8367bbcfe5f01
probiss <bulut.cihad@gmail.com>
2023-09-17 20:36:50 +0300

    first step for blog

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 const PORT = 5000;
 
 app.get('/', (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog);
-  })
-  
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  })
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
