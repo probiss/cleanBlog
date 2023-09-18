3ee7fec9887412408e6f7e960d7599402b1175e2
probiss <bulut.cihad@gmail.com>
2023-09-18 16:09:59 +0300

    connected to mongo and create schema

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
-
+const Post = require('./models/Post');
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/post-dev')
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async(req, res) => {
+  const posts = await Post.find({})
+  res.render('posts',{
+    posts
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -21,6 +29,11 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
+
+app.post('/posts', async(req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');  
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
