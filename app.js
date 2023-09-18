3661b641237c8d70d996e587bb78464a6c534679
probiss <bulut.cihad@gmail.com>
2023-09-18 21:07:05 +0300

    endpoints completed.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
 const path = require('path');
 const Post = require('./models/Post');
-const app = express();
+const PostController = require('./controllers/postController');
+const PageController = require('./controllers/pageController');
+const app = express(); 
 
 mongoose.connect('mongodb://127.0.0.1:27017/post-dev')
 
@@ -12,36 +15,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method', {methods:['POST','GET']}));
+
+app.get('/', PostController.getAllPosts);
+app.get('/posts/:id', PostController.getPost);
+app.post('/posts', PostController.createPost);
+app.put('/posts/:id', PostController.updatePost);
+app.delete('/posts/:id', PostController.deletePost);
+app.get('/about', PageController.getAboutPage);
+app.get('/add', PageController.getAddPage);
+app.get('/posts/edit/:id', PageController.getEditPage);
+
+app.get('/about', PageController.getAboutPage);
+
+app.get('/add', PageController.getAddPage);
 
 const PORT = 5000;
-
-app.get('/', async(req, res) => {
-  const posts = await Post.find({})
-  res.render('posts',{
-    posts
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-
-app.get('/posts/:id', async(req, res) => {
-  const post = await Post.findById(req.params.id)
-  res.render('post', {
-    post
-  })
-});
-
-app.post('/posts', async(req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');  
-})
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
